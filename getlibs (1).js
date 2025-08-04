d8053193d026f1d64817246ef312dd2c9457cfea
AnyaGuptaCoder <109542126+AnyaGuptaCoder@users.noreply.github.com>
2025-08-04 12:51:17 -0400

    Add files via upload

---
+++ getlibs (1).js
@@ -0,0 +1,82 @@
+function getLibs1() {
+		const a = document.getElementById('a').value.trim();
+		const b = document.getElementById('b').value.trim();
+		const c = document.getElementById('c').value.trim();
+		const d = document.getElementById('d').value.trim();
+		const e = document.getElementById('e').value.trim();
+		const f = document.getElementById('f').value.trim();
+		const g = document.getElementById('g').value.trim();
+		const h = document.getElementById('h').value.trim();
+		const i = document.getElementById('i').value.trim();
+
+		const story = `Once upon a time, there was a(n) ${a}. This ${a} was very ${b} and loved to ${c}. One day, while doing so, it tripped over a(n) ${d} and promptly ${e}. The ${d} woke up from its 1000 year nap and started ${f} at the ${a}. The ${a} was very ${g} and started ${h} away, but it was no use. The ${d} threw a(n) ${i} at the ${a} and the ${a}, finally having some sense knocked into it, was no longer ${g}. The end.`;
+
+		document.getElementById('storyOutput').textContent = story;
+}
+
+function getLibs2() {
+		const a = document.getElementById('a').value.trim();
+		const b = document.getElementById('b').value.trim();
+		const c = document.getElementById('c').value.trim();
+		const d = document.getElementById('d').value.trim();
+		const e = document.getElementById('e').value.trim();
+		const f = document.getElementById('f').value.trim();
+		const g = document.getElementById('g').value.trim();
+		const h = document.getElementById('h').value.trim();
+		const i = document.getElementById('i').value.trim();
+
+		const story = `It was a very ${a} day. It was so ${a}, that a ${b} happened to ${c}. Before this, however, the ${b} was ${d} and decidng to ${c} was unfortunately a terrible iea. The ${b} took ${e} laxatives as a result and its stomach started feeling extremely ${f}. Luckily, a(n) ${g} was nearby and saved the ${b} from its demise. The ${b} ${h} for joy and the ${g} was very happy, and ${i} in response. The end.`;
+
+		document.getElementById('storyOutput').textContent = story;
+}
+
+function getLibs3() {
+
+		const a = document.getElementById('a').value.trim();
+		const b = document.getElementById('b').value.trim();
+		const c = document.getElementById('c').value.trim();
+		const d = document.getElementById('d').value.trim();
+		const e = document.getElementById('e').value.trim();
+		const f = document.getElementById('f').value.trim();
+		const g = document.getElementById('g').value.trim();
+		const h = document.getElementById('h').value.trim();
+		const i = document.getElementById('i').value.trim();
+
+		const story = `One day, a(n) ${a} decided that it would go on a ${b} mile long walk. However, it decided to ${c} halfway through! This action wasted 5 hours, and it got dark before the ${a} could get home. By this time, the ${a} was very ${d} and decided to ${e} for warmth. It found a(n) ${f} and was very happy. The ${f} was very ${g} and they both, after very ${h} contemplation, went ${i} together. The end.`;
+
+		document.getElementById('storyOutput').textContent = story;
+}
+
+function getLibs4() { 
+
+		const a = document.getElementById('a').value.trim();
+		const b = document.getElementById('b').value.trim();
+		const c = document.getElementById('c').value.trim();
+		const d = document.getElementById('d').value.trim();
+		const e = document.getElementById('e').value.trim();
+		const f = document.getElementById('f').value.trim();
+		const g = document.getElementById('g').value.trim();
+		const h = document.getElementById('h').value.trim();
+		const i = document.getElementById('i').value.trim();
+
+		const story = `It was 1000 years since the last ${a} had ${b}. It was very peaceful until... ${c} leaves ominously swept through the ${d}. Suddenly, a(n) ${a} ${e} into the air! What a magical moment! And then... it ${b}. Everyone was ${g} at the scene. They were very ${i} and celebrated at the first time the ${a} had ${b} in 1000 years. The end.`;
+
+		document.getElementById('storyOutput').textContent = story;
+}
+
+function getLibs5() {
+
+		const a = document.getElementById('a').value.trim();
+		const b = document.getElementById('b').value.trim();
+		const c = document.getElementById('c').value.trim();
+		const d = document.getElementById('d').value.trim();
+		const e = document.getElementById('e').value.trim();
+		const f = document.getElementById('f').value.trim();
+		const g = document.getElementById('g').value.trim();
+		const h = document.getElementById('h').value.trim();
+		const i = document.getElementById('i').value.trim();
+
+		const story = `One day, a(n) ${a} decided to ${b}. It was a very nice day, and the ${a} even found ${c} gold coins on the ground! It ran to a nearby ${d} and ${e} inside. The ${a} was very happy and gave a(n) ${f} some of the gold coins it found. The ${f} was very ${g} and ${h} for joy, accidentally knocking over a(n) ${i} along the way. Oops. Unfortunately, the ${a} and the ${f} had to pay all the gold coins to repair the damage. The end! `;
+
+		document.getElementById('storyOutput').textContent = story;
+}
---
+++ makelibs (1).js
@@ -0,0 +1,55 @@
+function makeLibs1(){
+	document.getElementById('A').textContent = `Enter a noun:`;
+	document.getElementById('B').textContent = `Enter an adjective:`;
+	document.getElementById('C').textContent = `Enter a verb (ex. jump):`;
+	document.getElementById('D').textContent = `Enter a noun:`;
+	document.getElementById('E').textContent = `Enter a past tense verb (ex. 'waved'):`;
+	document.getElementById('F').textContent = `Enter a present tense verb (ex. 'flying'):`;
+	document.getElementById('G').textContent = `Enter an adjective:`;
+	document.getElementById('H').textContent = `Enter a present tense verb (ex. 'flying'):`;
+	document.getElementById('I').textContent = `Enter a noun:`;
+}
+function makeLibs2(){
+	document.getElementById('A').textContent = `Enter an adjective:`;
+	document.getElementById('B').textContent = `Enter a noun:`;
+	document.getElementById('C').textContent = `Enter a verb (ex. jump):`;
+	document.getElementById('D').textContent = `Enter a present tense verb (ex. 'flying'):`;
+	document.getElementById('E').textContent = `Enter a number:`;
+	document.getElementById('F').textContent = `Enter an adjective:`;
+	document.getElementById('G').textContent = `Enter a noun:`;
+	document.getElementById('H').textContent = `Enter a past tense verb (ex. 'waved'):`;
+	document.getElementById('I').textContent = `Enter a past tense verb (ex. 'waved'):`;
+}
+function makeLibs3(){
+	document.getElementById('A').textContent = `Enter a noun:`;
+	document.getElementById('B').textContent = `Enter a number`;
+	document.getElementById('C').textContent = `Enter a verb (ex. jump):`;
+	document.getElementById('D').textContent = `Enter an adjective:`;
+	document.getElementById('E').textContent = `Enter a verb (ex. jump):`;
+	document.getElementById('F').textContent = `Enter a noun:`;
+	document.getElementById('G').textContent = `Enter an adjective:`;
+	document.getElementById('H').textContent = `Enter an adjective:`;
+	document.getElementById('I').textContent = `Enter a present tense verb (ex. 'flying'):`;
+}
+function makeLibs4(){
+	document.getElementById('A').textContent = `Enter a past tense verb (ex. 'waved'):`;
+	document.getElementById('B').textContent = `Enter a noun:`;
+	document.getElementById('C').textContent = `Enter a number:`;
+	document.getElementById('D').textContent = `Enter a location:`;
+	document.getElementById('E').textContent = `Enter a past tense verb (ex. waved):`;
+	document.getElementById('F').textContent = `Enter a present tense verb (ex. 'flying'):`;
+	document.getElementById('G').textContent = `Enter an adjective:`;
+	document.getElementById('H').textContent = `Enter a noun:`;
+	document.getElementById('I').textContent = `Enter an adjective:`;
+}
+function makeLibs5(){
+	document.getElementById('A').textContent = `Enter a noun:`;
+	document.getElementById('B').textContent = `Enter a verb (ex. jump):`;
+	document.getElementById('C').textContent = `Enter a number:`;
+	document.getElementById('D').textContent = `Enter a location:`;
+	document.getElementById('E').textContent = `Enter a past tense verb (ex. 'waved'):`;
+	document.getElementById('F').textContent = `Enter a noun:`;
+	document.getElementById('G').textContent = `Enter an adjective:`;
+	document.getElementById('H').textContent = `Enter a past tense verb (ex. 'waved'):`;
+	document.getElementById('I').textContent = `Enter a noun:`;
+}
